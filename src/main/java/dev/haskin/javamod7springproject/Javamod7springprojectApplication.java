10066c8078d5c2b80c5623fa84e1893efc0c08c1
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-10 15:55:08 -0400

    initial commit

---
+++ src/main/java/dev/haskin/javamod7springproject/Javamod7springprojectApplication.java
@@ -0,0 +1,13 @@
+package dev.haskin.javamod7springproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javamod7springprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javamod7springprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/haskin/javamod7springproject/Javamod7springprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.haskin.javamod7springproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javamod7springprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
