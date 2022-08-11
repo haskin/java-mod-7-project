6cecf96a02b9b1c9f0e5f8d91875ac199d8fae40
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-11 13:06:45 -0400

    removed white spaces in application.properties (bug)

--- src/main/java/dev/haskin/javamod7springproject/model/User.java
+++ src/main/java/dev/haskin/javamod7springproject/model/User.java
@@ -2,16 +2,22 @@ package dev.haskin.javamod7springproject.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
 @Table(name = "\"User\"")
-@Entity
+// @Entity
 @Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    private String username;
+    @NotNull
+    private String password;
 }
