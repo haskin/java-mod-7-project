2d37dfb8aba3e04762d83e484111e0100e39942a
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-11 14:58:02 -0400

    Removed automatic date for published

--- src/main/java/dev/haskin/javamod7springproject/model/Book.java
+++ src/main/java/dev/haskin/javamod7springproject/model/Book.java
@@ -2,9 +2,7 @@ package dev.haskin.javamod7springproject.model;
 
 import java.time.LocalDate;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -15,14 +13,12 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Table(name = "Book")
 @Entity
-@EntityListeners(AuditingEntityListener.class)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Data
 public class Book {
@@ -37,7 +33,6 @@ public class Book {
     private int pages;
     @NotNull
     @LastModifiedDate
-    @Column(columnDefinition = "DATE")
     private LocalDate published;
     @NotNull
     @ManyToOne
