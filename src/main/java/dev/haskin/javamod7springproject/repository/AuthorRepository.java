ea615e60a4cdbf5187a561e54738a523a98b5541
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-11 15:07:47 -0400

    created author repository

---
+++ src/main/java/dev/haskin/javamod7springproject/repository/AuthorRepository.java
@@ -0,0 +1,11 @@
+package dev.haskin.javamod7springproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import dev.haskin.javamod7springproject.model.Author;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+}
