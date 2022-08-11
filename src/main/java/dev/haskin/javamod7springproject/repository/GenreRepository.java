cbc3d6fb46217938df5c433a2d2ce218d40ac035
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-11 15:14:27 -0400

    Created genre repository

--- src/main/java/dev/haskin/javamod7springproject/repository/GenreRepository.java
+++ src/main/java/dev/haskin/javamod7springproject/repository/GenreRepository.java
@@ -3,9 +3,9 @@ package dev.haskin.javamod7springproject.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import dev.haskin.javamod7springproject.model.Book;
+import dev.haskin.javamod7springproject.model.Genre;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface GenreRepository extends JpaRepository<Genre, Long> {
 
 }
