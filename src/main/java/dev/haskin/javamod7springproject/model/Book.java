a5c13296e9c74c9145065766aca18cb9386271c7
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-11 13:33:12 -0400

    created book entity

--- src/main/java/dev/haskin/javamod7springproject/model/Book.java
+++ src/main/java/dev/haskin/javamod7springproject/model/Book.java
@@ -1,23 +1,31 @@
 package dev.haskin.javamod7springproject.model;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
-@Table(name = "\"User\"")
+@Table(name = "Book")
 @Entity
 @Data
-public class User {
+public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotNull
-    private String username;
+    private String title;
     @NotNull
-    private String password;
+    @Min(1)
+    private int pages;
+    @NotNull
+    @Column(columnDefinition = "DATE")
+    private LocalDate published;
 }
