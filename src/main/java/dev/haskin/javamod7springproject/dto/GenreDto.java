bd3e19102737a4806419add21fa8b2337a4f201a
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-12 14:03:44 -0400

    created Genre Dto

---
+++ src/main/java/dev/haskin/javamod7springproject/dto/GenreDto.java
@@ -0,0 +1,16 @@
+package dev.haskin.javamod7springproject.dto;
+
+import javax.validation.constraints.NotEmpty;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class GenreDto {
+    private Long id;
+    @NotEmpty
+    private String name;
+}
